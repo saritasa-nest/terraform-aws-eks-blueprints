d44f46ad2b590e25875a8a7965bc014bbfed237f
Luciano Buono <luciano.buono@saritasa.com>
2025-03-28 12:09:40 -0300

    feat: add chart_value_files parameter SD-1149

--- modules/kubernetes-addons/argocd/main.tf
+++ modules/kubernetes-addons/argocd/main.tf
@@ -82,6 +82,14 @@ resource "helm_release" "argocd_application" {
     ]
   }
 
+  set_list {
+    name = "source.plugin.chart_value_files"
+    value = length(try(each.value.chart_value_files, [])) > 0 ? each.value.chart_value_files : [
+      "values*.yaml",
+      "values*.yml",
+    ]
+  }
+
   # Destination Config.
   set {
     name  = "destination.server"
